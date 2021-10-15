ca76c7aea372e6bc9290493084d8c6bfdbb7b708
ThinkBlackInk <app@think-black-ink.com>
2021-10-15 11:04:38 -0400

    New Build

--- www/_instantUpdateSettings.js
+++ www/_instantUpdateSettings.js
@@ -1,6 +1,6 @@
 function _instantUpdateSettings() {
 return {
-	"baseLineGUID": "3303f06ce1914a90aa5bd9fb74a8c54d",
+	"baseLineGUID": "09bfc16e341a4d1cb1f9b7c11a5b0165",
 	"baseURL": "https://phonegapinstantupdate.s3.amazonaws.com/",
 	"displayMessageAfterUpdate": false,
 	"message": "",
